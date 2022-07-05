8c2de89cb7b20d5e53a829cbfc9bca8d2ed56477
Fredrik Johansson <jeanfredrik@gmail.com>
2022-07-05 20:56:04 +0200

    feat: Add `whitespace-headless-cms/disable_comments` filter

--- autoload/disable-features.php
+++ autoload/disable-features.php
@@ -1,55 +1,63 @@
 <?php
 
-/**
- * Disable comments
- */
-add_action("admin_init", function () {
-  // Redirect any user trying to access comments page
-  global $pagenow;
+add_action("plugins_loaded", function () {
+  $disable_comments = apply_filters(
+    "whitespace-headless-cms/disable_comments",
+    true,
+  );
+  if ($disable_comments) {
+    /**
+     * Disable comments
+     */
+    add_action("admin_init", function () {
+      // Redirect any user trying to access comments page
+      global $pagenow;
 
-  if ($pagenow === "edit-comments.php") {
-    wp_redirect(admin_url());
-    exit();
-  }
+      if ($pagenow === "edit-comments.php") {
+        wp_redirect(admin_url());
+        exit();
+      }
 
-  // Remove comments metabox from dashboard
-  remove_meta_box("dashboard_recent_comments", "dashboard", "normal");
+      // Remove comments metabox from dashboard
+      remove_meta_box("dashboard_recent_comments", "dashboard", "normal");
 
-  // Disable support for comments and trackbacks in post types
-  foreach (get_post_types() as $post_type) {
-    if (post_type_supports($post_type, "comments")) {
-      remove_post_type_support($post_type, "comments");
-      remove_post_type_support($post_type, "trackbacks");
-    }
-  }
-  if (is_admin_bar_showing()) {
-    remove_action("admin_bar_menu", "wp_admin_bar_comments_menu", 60);
-  }
-});
+      // Disable support for comments and trackbacks in post types
+      foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, "comments")) {
+          remove_post_type_support($post_type, "comments");
+          remove_post_type_support($post_type, "trackbacks");
+        }
+      }
+      if (is_admin_bar_showing()) {
+        remove_action("admin_bar_menu", "wp_admin_bar_comments_menu", 60);
+      }
+    });
 
-// Close comments on the front-end
-add_filter("comments_open", "__return_false", 20, 2);
-add_filter("pings_open", "__return_false", 20, 2);
+    // Close comments on the front-end
+    add_filter("comments_open", "__return_false", 20, 2);
+    add_filter("pings_open", "__return_false", 20, 2);
 
-// Hide existing comments
-add_filter("comments_array", "__return_empty_array", 10, 2);
+    // Hide existing comments
+    add_filter("comments_array", "__return_empty_array", 10, 2);
 
-// Remove comments page in menu
-add_action("admin_menu", function () {
-  remove_menu_page("edit-comments.php");
-});
+    // Remove comments page in menu
+    add_action("admin_menu", function () {
+      remove_menu_page("edit-comments.php");
+    });
 
-// Remove comments links from admin bar
-add_action("init", function () {
-  if (is_admin_bar_showing()) {
-    remove_action("admin_bar_menu", "wp_admin_bar_comments_menu", 60);
+    // Remove comments links from admin bar
+    add_action("init", function () {
+      if (is_admin_bar_showing()) {
+        remove_action("admin_bar_menu", "wp_admin_bar_comments_menu", 60);
+      }
+    });
   }
 });
 
 /*
  * Remove footer text
  */
-add_filter("admin_footer_text", function() {});
+add_filter("admin_footer_text", function () {});
 
 /*
  * Disable json rest api if WP GraphQL is enabled
